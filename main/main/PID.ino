077d3eaa6c2b3d622b7b21a5c02a6d4f1f0efe64
SNEHAPALANIAPPAN <61728834+SNEHAPALANIAPPAN@users.noreply.github.com>
2020-03-04 11:09:04 +0800

    Add files via upload

---
+++ main/main/PID.ino
@@ -0,0 +1,37 @@
+const double KP = 4;
+const double KI = 2;
+const double KD = 0.05;
+
+
+
+
+double computePID() {   //Proportional, Integral,Derivative. 
+// source: https://www.teachmemicro.com/arduino-pid-control-tutorial/
+  double p, i, d, pid, error, integral;   //kp->proportional, ki->integral, kd->deriavative
+
+  error = deltaM1Ticks - deltaM2Ticks; // originally was m1ticks - m2ticks, which is actually cumulative error (without taking into account time) because m1ticks and m2ticks are cumulative.
+  cumError = m1Ticks - m2Ticks;
+
+  p = KP * error;
+  i = KI * cumError;
+  d = KD * (error - lastError);
+  
+  pid = p + i + d;
+  
+  lastError = error;
+
+
+  //Return PID that is within 50<-->-50range
+  if (pid >= 50){
+    return 50;
+    }
+  else if (pid <= -50){
+    return -50;
+    }
+  else{
+    return pid;
+    }
+
+ 
+  
+}
---
+++ main/main/loop.ino
@@ -0,0 +1,145 @@
+/*
+Control command send to serial port:
+
+prefix: 'f' -> move forward
+        'b' -> move backward
+        'l' -> trun left
+        'r' -> turn right
+
+siffix: distance in cm or degreee in °
+
+
+eg,
+      f100 -> move forward 100cm
+      b40  -> move back 40cm
+      l70  -> turn left 70°
+      r60  -> turn right 60°
+
+*/
+
+
+
+
+char cmd ='#';
+String inString = "";  // string to hold input
+int distance = 0;
+
+
+//Direction Information
+//const int DIRECTION_FORWARD[2] = {1, -1};
+//const int DIRECTION_BACKWARD[2] = {-1, 1};
+//const int DIRECTION_LEFT[2] = {1, 1};
+//const int DIRECTION_RIGHT[2] = {-1, -1};
+
+
+void loopReading(){
+
+  
+  
+  while (Serial.available() > 0) {
+    if (cmd != '#'){
+      int inChar = Serial.read();
+      if (isDigit(inChar)) {
+        // convert the incoming byte to a char and add it to the string:
+        inString += (char)inChar;
+      }
+      // if you get a newline, print the string, then the string's value:
+      if (inChar == '\n') {
+
+        Serial.print("Value:");
+        Serial.println(inString.toInt());
+        distance = inString.toInt();
+        switch(cmd){
+          case 'f':{
+            move(convertDistanceToTicks(distance),  DIRECTION_FORWARD);
+            //moveWithSpeed(convertDistanceToTicks(distance),  DIRECTION_FORWARD, 150);
+
+            Serial.println("Xdone");
+            break;
+          }
+          case 'b':{
+            move(convertDistanceToTicks(distance),   DIRECTION_BACKWARD);
+            Serial.println("Xdone");
+            
+            break;
+          }
+          case 'l':{
+            move(convertLeftAngleToTicks(distance), DIRECTION_LEFT);
+            Serial.println("Xdone");
+            
+            break;
+          }
+          case 'r':{
+            move(convertRightAngleToTicks(distance), DIRECTION_RIGHT);
+            Serial.println("Xdone");
+            
+            break;
+          }
+
+          case 's':{
+            checkRawValues ();
+           
+
+            break;
+          }
+
+          case 'k':{
+            navObs(convertDistanceToTicks(100),DIRECTION_FORWARD);
+             Serial.println("Xdone");
+            
+            break;
+          }
+
+          case 'i':{
+            caliFrontLeft();
+             Serial.println("Xdone");
+            
+            break;
+          }
+
+          case 'o':{
+            caliFront();
+             Serial.println("Xdone");
+            
+            break;
+          }
+
+          case 'p':{
+            caliLeft();
+             Serial.println("Xdone");
+            
+            break;
+          }
+
+          case 'u':{
+            caliLeftBack();
+             Serial.println("Xdone");
+            
+            break;
+          }
+          case 'n':{
+            caliNew();
+             Serial.println("Xdone");
+            
+            break;
+          }
+        }
+       
+        // clear the string for new input:
+        cmd ='#';
+        inString = "";  // string to hold input
+        distance = 0;
+        
+      }
+    }
+    else {
+      cmd = (char)Serial.read();
+      
+     
+    }
+    
+  }
+  Serial.flush();
+  
+  
+}
