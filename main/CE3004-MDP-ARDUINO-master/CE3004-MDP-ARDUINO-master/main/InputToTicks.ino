8a2c91b6450aed76e78dbb86edcb1f128b915b3d
SNEHAPALANIAPPAN <61728834+SNEHAPALANIAPPAN@users.noreply.github.com>
2020-03-04 10:18:37 +0800

    Add files via upload

---
+++ main/CE3004-MDP-ARDUINO-master/CE3004-MDP-ARDUINO-master/main/InputToTicks.ino
@@ -0,0 +1,22 @@
+// TODO: need to recalibrate this before every run
+double convertDistanceToTicks(int distance) //10cm
+{
+  // calculation was made with the knowledge that one revolution of the wheel is 510 ticks, and the diameter of the wheel is 6cm.
+  // https://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_drive_some_distance
+  double ticks = distance*29.3;   //277 wk9:270      6.3vB2: 242
+  return ticks;
+}
+
+
+
+double convertLeftAngleToTicks(int angle)
+{
+  double tick = angle *4.40 ; // 332-6.17VB2   6.3vB2: 332
+  return tick;
+}
+
+double convertRightAngleToTicks(int angle)
+{
+  double tick = angle * 4.40; // 339 -6.17VB2     6.3vB2: 340
+  return tick;
+}
---
+++ main/CE3004-MDP-ARDUINO-master/CE3004-MDP-ARDUINO-master/main/Move.ino
@@ -0,0 +1,110 @@
+int move(double ticks, const int direction[2]) 
+{
+    resetGlobalConstants();
+    double pid = 0;
+    double brakingOffset = 0;
+
+
+    md.setSpeeds((SPEED + pid) * direction[0]  , (SPEED - pid) * direction[1]);
+    while (m1Ticks <= ticks)
+    {   
+
+        computeDelta();
+        pid = computePID();
+
+       
+        double difference = ticks - m1Ticks;
+        if ( difference < 120) {
+          brakingOffset = 2 * (120 - difference);
+          md.setSpeeds((SPEED + pid - brakingOffset) * direction[0], (SPEED - pid - brakingOffset) * direction[1]);
+        } else {
+          md.setSpeeds((SPEED + pid) * direction[0], (SPEED - pid) * direction[1]);
+        }
+
+        
+
+        
+        md.setSpeeds((SPEED + pid) * direction[0], (SPEED - pid) * direction[1]);
+            
+//             Serial.println("printing number of ticks");
+//             Serial.println(deltaM1Ticks);
+//             Serial.println(m1Ticks);
+//             Serial.println(m2Ticks);
+        // delay(70);
+
+        //delay(1);
+
+        //Serial.println();       
+    }
+    md.setBrakes(400, 400);
+ //    the number of ticks output here should be the same for each motor
+//    Serial.print("#final m1ticks: ");
+//    Serial.print(m1Ticks);
+//    Serial.print(" #final m2ticks: ");
+//    Serial.println(m2Ticks);
+
+    //delay(200);
+  
+   // Serial.print("# final m2ticks after delay: ");
+   // Serial.println(m2ticks);
+
+//    Serial.print("#sum of deltaM1Ticks: ");
+//    Serial.print(sumOfDeltaM1);
+//    Serial.print("# sum of deltaM2Ticks: ");
+//    Serial.println(sumOfDeltaM2);
+
+    delay(50);
+
+    return 1;
+}
+
+
+int moveWithSpeed(double ticks, const int direction[2], int speed) 
+{
+    resetGlobalConstants();
+    double pid = 0;
+
+
+    md.setSpeeds((speed + pid) * direction[0]  , (speed - pid) * direction[1]);
+    while (m1Ticks <= ticks)
+    {   
+
+        computeDelta();
+        pid = computePID();
+
+
+
+
+        
+            
+//             Serial.println("printing number of ticks");
+//             Serial.println(deltaM1Ticks);
+//             Serial.println(m1Ticks);
+//             Serial.println(m2Ticks);
+        // delay(70);
+
+        //delay(1);
+
+        //Serial.println();       
+    }
+    md.setBrakes(400, 400);
+ //    the number of ticks output here should be the same for each motor
+//    Serial.print("#final m1ticks: ");
+//    Serial.print(m1Ticks);
+//    Serial.print(" #final m2ticks: ");
+//    Serial.println(m2Ticks);
+
+    //delay(200);
+  
+   // Serial.print("# final m2ticks after delay: ");
+   // Serial.println(m2ticks);
+
+//    Serial.print("#sum of deltaM1Ticks: ");
+//    Serial.print(sumOfDeltaM1);
+//    Serial.print("# sum of deltaM2Ticks: ");
+//    Serial.println(sumOfDeltaM2);
+
+    delay(50);
+
+    return 1;
+}
