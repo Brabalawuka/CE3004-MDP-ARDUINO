98bdeca2b76f1c65be8b502fcdfbfdbdd599a95a
Limzui <37795828+Limzui@users.noreply.github.com>
2020-03-18 14:26:18 +0800

    Fix bug with calinew (Always using IR3 cali because no break in switch)

--- main/InputToTicks.ino
+++ main/InputToTicks.ino
@@ -3,7 +3,8 @@ double convertDistanceToTicks(int distance) //10cm
 {
   // calculation was made with the knowledge that one revolution of the wheel is 510 ticks, and the diameter of the wheel is 6cm.
   // https://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_drive_some_distance
-  double ticks = distance * (30.3 + distance/150);      //6.21v 28.8f
+  double ticks = distance * (29.8 + distance/150);
+  //double ticks = distance * (30.3 + distance/150);      //6.21v 28.8f
   // double ticks = distance * (28.7 + distance/100); 
   return ticks;
 }
